461a151861a81cf5e95c40393a18b9b90cd898a0
kLiHz <66966137+kLiHz@users.noreply.github.com>
2023-09-09 01:54:26 +0800

    Temp run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+curl -LO 'https://www.python.org/ftp/python/3.12.0/Python-3.12.0rc2.tgz'
+tar xf *.tgz
+cd Python-3.120rc2
+mkdir install
+sudo apt install -y llvm llvm-dev clang
+
+CC=clang CXX=clang++ ./configure --with-lto=thin --enable-optimization --prefix="$PWD/install/"
+make
+make install
+tar --zstd -cf python3.12.0rc2.tar.zstd
