e375dd44031b642a78b0d6bb926236ae9ada7ea8
kLiHz <66966137+kLiHz@users.noreply.github.com>
2023-12-12 06:37:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
-curl -LO 'https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz'
-tar xf *.tgz
-cd Python-3.12.0
+curl -LO 'https://www.python.org/ftp/python/3.12.1/Python-3.12.1.tar.xz'
+tar xf Python-3.12.1.tar.xz
+cd Python-3.12.1
 mkdir install
 sudo apt install -y llvm llvm-dev clang
 
 CC=clang CXX=clang++ ./configure --with-lto=thin --enable-optimizations --prefix="$PWD/install/"
 make
 make install
-tar --zstd -cf python3.12.0.tar.zst install
+mv install python3121
+tar --zstd -cf python3.12.1.tar.zst python3121
