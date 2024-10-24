272b9c6674a6bc1194014ba7b2f83a75f2d5543d
kLiHz <66966137+kLiHz@users.noreply.github.com>
2024-10-25 04:10:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
-# VER="3.13.0"
-# MINOR="b1"
-# VERSION="${VER}${MINOR}"
-
-VER="3.12.2"
-MINOR=""
+VER="3.14.0"
+MINOR="a1"
 VERSION="${VER}${MINOR}"
 
 curl -L "https://www.python.org/ftp/python/${VER}/Python-${VER}${MINOR}.tgz" | tar -xz
@@ -12,11 +8,18 @@ cd "Python-${VERSION}"
 OUTDIR="python-$VERSION"
 mkdir $OUTDIR
 
-sudo apt install pkg-config libssl-dev libsqlite3-dev libbz2-dev liblzma-dev tk-dev libreadline-dev libgdbm-dev python3-gdbm tcl-tclreadline libqdbm-dev libgdbm-compat-dev
+sudo apt install -y pkg-config libssl-dev libsqlite3-dev libbz2-dev liblzma-dev tk-dev libreadline-dev libgdbm-dev python3-gdbm tcl-tclreadline libqdbm-dev libgdbm-compat-dev
+
+sudo apt install -y gpg
+curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+echo 'deb http://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-18 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+echo 'deb-src http://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-18 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+sudo apt update
 
 # sudo apt install -y llvm llvm-dev clang
 # CC=clang CXX=clang++ ./configure --with-lto=thin --enable-optimizations --prefix="$PWD/install/"
-sudo apt install clang-16
+
+sudo apt install -y clang-18
 ./configure --enable-optimizations --prefix="$PWD/$OUTDIR/" --enable-experimental-jit
 make
 make install
